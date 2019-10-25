c4716daf5129fa7df60b476733d0c0fa530191de
spencer.strickland <spencerstrickland1@hotmail.com>
2019-10-24 19:11:56 -0700

    whitespace to make things more readable

--- QuestieLuaUnitTests.lua
+++ QuestieLuaUnitTests.lua
@@ -12,11 +12,14 @@ function testTableSorting()
         [331] = "Ashenvale",
     }
     SortedKeys = questieJourney.DrawZoneQuestTab("null", LeftSideOfMap)
+    
     --Assert Ashenvale got moved into the first positon of the sorted keys
     luaunit.assertEquals(LeftSideOfMap[SortedKeys[1]], "Ashenvale")
+
     --Assert Dustwallow becomes the last position of the sorted keys
     luaunit.assertEquals(LeftSideOfMap[SortedKeys[7]], "Dustwallow Marsh")
 end
 
 testTableSorting()
+
 os.exit( luaunit.LuaUnit.run() )
