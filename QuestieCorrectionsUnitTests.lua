5d295cca5a44cd4b82b86e3ba4103a9d3cb86094
Spencer0 <spencerstrickland1@hotmail.com>
2019-10-27 19:49:35 -0700

    Wrote unit tests for PR#3

---
+++ QuestieCorrectionsUnitTests.lua
@@ -0,0 +1,24 @@
+luaunit = require('luaunit')
+questieFixes = require('QuestieQuestFixes')
+
+function testQuestCorrections()
+    questTable = questieFixes.Load()
+    -- The quest we removed from the corrections file
+    removedKey = 1282
+
+    -- Flag that toggles if we find the corrections in the file
+    correctionFound = false
+
+    --Iterate over the list of quest IDs and compare values
+    for k,v in pairs(questTable) do
+        if(k==removedKey)then 
+            correctionFound = true
+        end
+    end
+    luaunit.assertFalse(correctionFound)
+      
+end
+
+testQuestCorrections()
+
+os.exit( luaunit.LuaUnit.run() )
